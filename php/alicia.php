25e582ae2b9ae4e03fe71e215b84e01fd000c427
Jack1289 <95daniel.jack@gmail.com>
2016-05-17 19:16:02 -0600

    n

---
+++ php/alicia.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 3, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+<body  background="../img/h.jpg">
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <nav class="navbar navbar-default navbar-inverse" role="navigation">
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                    </button> <a class="navbar-brand" href="../index.html">Cinemas Delicias</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+
+                        <li>
+                            <a href="pelicula.php">CARTELERA</a>
+                        </li>
+
+                    </ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-default">
+                            Submit
+                        </button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+    </div>
+</div>
+<!--<iframe width="560" height="315" src="https://www.youtube.com/embed/_gBLh7IrAvU" frameborder="0" allowfullscreen></iframe>-->
+
+<div align="center" >
+
+
+    <br>
+    <img src="../img/xmen.jpg" width="400px" style="float: left; padding-left: 5%" >
+
+
+
+
+
+</div>
+<font color="white">
+<label>Película: </label><label> Alicia atravez del espejo</label><br>
+<label>Duración: </label><label> 147 min</label><br>
+<label>Género: </label><label>Aventura y Fantasia</label><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<label style="font-size: large; padding-left: 5%">Horario</label></br>
+<label  size="6" style="width: 50%; padding-left: 5%" >Desde los albores de la civilización, fue adorado como un dios. Apocalypse, el primer y más poderoso mutante del universo X-Men de Marvel, amasó los poderes de muchos otros mutantes, volviéndose inmortal e invencible. Al despertar después de miles de años, se siente desilusionado con el mundo tal y como lo encuentra, y recluta a un equipo de poderosos mutantes, incluyendo un descorazonado Magneto (Michael Fassbender), para purificar a la humanidad y crear un nuevo orden mundial sobre el cual reinará. Al pender de un hilo el destino de la Tierra, Raven (Jennifer Lawrence) con la ayuda del Profesor X (James McAvoy) debe capitanear a un equipo de jóvenes X-Men, para detener a su mayor némesis y salvar a la humanidad de su completa destrucción. </label>
+</font>
+<div align="center" >
+
+
+
+    <video width="800" height="533" controls>
+        <source src="../video/Alicia.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+
+
+</div>
+
+
+<script src="../js/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/scripts.js"></script>
+</body>
+</html>
---
+++ php/bestias.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 3, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+<body background="../img/h.jpg" >
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <nav class="navbar navbar-default navbar-inverse" role="navigation">
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                    </button> <a class="navbar-brand" href="../index.html">Cinemas Delicias</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+
+                        <li>
+                            <a href="pelicula.php">CARTELERA</a>
+                        </li>
+
+                    </ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-default">
+                            Submit
+                        </button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+    </div>
+</div>
+<!--<iframe width="560" height="315" src="https://www.youtube.com/embed/_gBLh7IrAvU" frameborder="0" allowfullscreen></iframe>-->
+
+<div align="center" >
+
+
+    <br>
+    <img src="../img/animales-fantasticos.jpg" width="400px" style="float: left; padding-left: 5%" >
+
+
+
+
+
+</div>
+<font color="white">
+<label >Película: </label>
+<label> Animales fantásticos y dónde encontrarlos</label><br>
+<label>Duración: </label><label> 160 min</label><br>
+<label>Género: </label><label>Aventura y fantasia</label><br>
+<label>Clasificacion:</label><label> </label>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<label style="font-size: large; padding-left: 5%">Horario</label></br>
+<label  size="6" style="width: 50%; padding-left: 5%" >La historia comienza en 1926 cuando Newt Scamander se prepara para iniciar una excursión por todo el mundo en busca de una extraordinaria variedad de criaturas mágicas. Al llegar a Nueva York para hacer una breve escala, todo está previsto para que no ocurra ningún incidente... si no fuera por un No-Maj (es la forma de llamar a los muggles en América, es decir, personas no mágicas) llamado Jacob, un caso fuera de lugar, y la fuga de algunas bestias fantásticas de Newt, que bien podría significar un verdadero problema tanto para la comunidad mágica como para los muggle.</label>
+    </font>
+<div align="center" >
+
+
+
+    <video width="800" height="533" controls>
+        <source src="../video/ANIMALES%20FANTÁSTICOS%20Y%20DÓNDE%20ENCONTRARLOS%20-%20Trailer%202%20-%20Oficial%20Warner%20Bros.%20Pictures.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+
+
+
+</div>
+
+
+<script src="../js/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/scripts.js"></script>
+</body>
+</html>
