f89b8a67e39fb1399996b02719f5d33cd045bb74
kurohappy <teddy14@hotmail.com>
2016-06-06 13:05:24 -0600

    br

--- php/boruto.php
+++ php/boruto.php
@@ -107,7 +107,7 @@
             mysqli_close($conn);
        ?>
     </select><br><br>
-    <label style="padding-left: 5%; font-size: large;">Sinopsis:</label><br>
+    <label style="padding-left: 5%; font-size: large;">Sinopsis:</label><br><br>
     <label  size="6" style="width: 50%; padding-left: 5%" ><?php echo $sentencia->fields('sinopsis'); ?></label>
 
 </font>
